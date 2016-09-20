bfd8fa1c66a215039353ffd5101a1b8cf930d27c
Phenix Yu <phenix.yu@gmail.com>
2016-09-20 17:53:23 +0800

    update to tvos10

--- TopShelf/ServiceProvider.swift
+++ TopShelf/ServiceProvider.swift
@@ -15,15 +15,15 @@ class ServiceProvider: NSObject, TVTopShelfProvider {
         super.init()
     }
     
-    let topShelfStyle: TVTopShelfContentStyle = .Sectioned
+    let topShelfStyle: TVTopShelfContentStyle = .sectioned
     
     let query = "http://api.redtube.com/?data=redtube.Videos.searchVideos&output=json&thumbsize=big&category=japanese"
     
     var topShelfItems: [TVContentItem] {
         var items: [TVContentItem] = []
-        let semaphore = dispatch_semaphore_create(0)
-        let url = NSURL(string: query)
-        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
+        let semaphore = DispatchSemaphore(value: 0)
+        let url = URL(string: query)
+        let task = URLSession.shared.dataTask(with: url!, completionHandler: {(data, response, error) in
             let sectionTitle = "RedTube"
             guard let sectionIdentifier = TVContentIdentifier(identifier: sectionTitle, container: nil)
                 else { fatalError("Error creating content identifier for section item.") }
@@ -37,25 +37,25 @@ class ServiceProvider: NSObject, TVTopShelfProvider {
                 sectionItem.topShelfItems?.append(self.contentItemWithJSON(video["video"]))
             }
             items.append(sectionItem)
-            dispatch_semaphore_signal(semaphore)
-        }
+            semaphore.signal()
+        }) 
         task.resume()
-        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER)
+        semaphore.wait(timeout: DispatchTime.distantFuture)
         return items;
     }
     
     /// Returns a `TVContentItem` for a `JSON`.
-    private func contentItemWithJSON(dataItem: JSON) -> TVContentItem {
+    fileprivate func contentItemWithJSON(_ dataItem: JSON) -> TVContentItem {
         guard let contentIdentifier = TVContentIdentifier(identifier: "video", container: nil)
             else { fatalError("Error creating content identifier.") }
         guard let contentItem = TVContentItem(contentIdentifier: contentIdentifier)
             else { fatalError("Error creating content item.") }
         
         contentItem.title = dataItem["title"].stringValue
-        contentItem.imageURL = NSURL(string: dataItem["thumb"].stringValue)
-        contentItem.displayURL = NSURL(string: "redtube://display/"+dataItem["video_id"].stringValue)
-        contentItem.playURL = NSURL(string: "redtube://play/"+dataItem["video_id"].stringValue)
-        contentItem.imageShape = TVContentItemImageShape.Wide
+        contentItem.imageURL = URL(string: dataItem["thumb"].stringValue)
+        contentItem.displayURL = URL(string: "redtube://display/"+dataItem["video_id"].stringValue)
+        contentItem.playURL = URL(string: "redtube://play/"+dataItem["video_id"].stringValue)
+        contentItem.imageShape = TVContentItemImageShape.wide
 
         print(contentItem.title)
         
--- redtube/AppDelegate.swift
+++ redtube/AppDelegate.swift
@@ -15,9 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDe
     var window: UIWindow?
     var appController: TVApplicationController?
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window = UIWindow(frame: UIScreen.main.bounds)
         
         /*
         Create the TVApplicationControllerContext for this application
@@ -35,14 +35,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDe
         the user quickly.
         */
         
-        if let javaScriptURL = NSBundle.mainBundle().URLForResource("application", withExtension: "js", subdirectory: "client/js") {
+        if let javaScriptURL = Bundle.main.url(forResource: "application", withExtension: "js", subdirectory: "client/js") {
             appControllerContext.javaScriptApplicationURL = javaScriptURL
         }
-        let TVBaseURL = appControllerContext.javaScriptApplicationURL.URLByDeletingLastPathComponent?.URLByDeletingLastPathComponent
+        let TVBaseURL = (appControllerContext.javaScriptApplicationURL as NSURL).deletingLastPathComponent?.deletingLastPathComponent()
         appControllerContext.launchOptions["BASEURL"] = TVBaseURL?.absoluteString
-        if let launchOptions = launchOptions as? [String: AnyObject] {
+        
+        if let launchOptions = launchOptions {
             for (kind, value) in launchOptions {
-                appControllerContext.launchOptions[kind] = value
+                appControllerContext.launchOptions[kind.rawValue] = value
             }
         }
         
@@ -53,57 +54,57 @@ class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDe
     
     // MARK: TVApplicationControllerDelegate
     
-    func appController(appController: TVApplicationController, didFinishLaunchingWithOptions options: [String: AnyObject]?) {
+    func appController(_ appController: TVApplicationController, didFinishLaunching options: [String: Any]?) {
         //print("\(__FUNCTION__) invoked with options: \(options)")
     }
     
-    func appController(appController: TVApplicationController, didFailWithError error: NSError) {
-        print("\(__FUNCTION__) invoked with error: \(error)")
+    func appController(_ appController: TVApplicationController, didFail error: Error) {
+        print("\(#function) invoked with error: \(error)")
         
         let title = "Error Launching Application"
         let message = error.localizedDescription
-        let alertController = UIAlertController(title: title, message: message, preferredStyle:.Alert )
+        let alertController = UIAlertController(title: title, message: message, preferredStyle:.alert )
         
-        self.appController?.navigationController.presentViewController(alertController, animated: true, completion: { () -> Void in
+        self.appController?.navigationController.present(alertController, animated: true, completion: { () -> Void in
             // ...
         })
     }
     
-    func appController(appController: TVApplicationController, didStopWithOptions options: [String: AnyObject]?) {
+    func appController(_ appController: TVApplicationController, didStop options: [String: Any]?) {
         //print("\(__FUNCTION__) invoked with options: \(options)")
     }
 
     // MARK: UIApplicationDelegate
     
-    func application(app: UIApplication, openURL url: NSURL, options: [String : AnyObject]) -> Bool {
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any]) -> Bool {
         if url.host == nil { return true }
         let args = url.absoluteString.characters.split{$0=="/"}.map(String.init)
-        appController?.evaluateInJavaScriptContext({ (evaluation:JSContext) -> Void in
+        appController?.evaluate(inJavaScriptContext: { (evaluation:JSContext) -> Void in
             evaluation.evaluateScript("queryVideoURL('/\(args[2])')")
             }, completion: { (b:Bool) -> Void in }
         )
         return true;
     }
 
-    func applicationWillResignActive(application: UIApplication) {
+    func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
-    func applicationDidEnterBackground(application: UIApplication) {
+    func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
-    func applicationWillEnterForeground(application: UIApplication) {
+    func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
-    func applicationDidBecomeActive(application: UIApplication) {
+    func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
-    func applicationWillTerminate(application: UIApplication) {
+    func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
